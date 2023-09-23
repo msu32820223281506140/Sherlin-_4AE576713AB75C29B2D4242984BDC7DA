96bbc464cace9b4c20dd3f156f3bed9f13156a6e
msu32820223281506140 <144217651+msu32820223281506140@users.noreply.github.com>
2023-09-23 09:19:21 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,20 +1,17 @@
-#Program to print factorial of a number
-# recursively.
- 
-# Recursive function
-def recursive_factorial(n):
-  if n == 1:
-      return n
-  else:
-      return n * recursive_factorial(n-1)
- 
-# user input
-num = 6
- 
-# check if the input is valid or not
-if num < 0:
-  print("Invalid input ! Please enter a positive number.")
-elif num == 0:
-  print("Factorial of number 0 is 1")
-else:
-  print("Factorial of number", num, "=", recursive_factorial(num))
+def sort_students(students):
+    students.sort(key=lambda x: x.CGPA, reverse=True)
+class Student:
+    def __init__(self, name, roll_number, CGPA):
+        self.name = name
+        self.roll_number = roll_number
+        self.CGPA = CGPA
+students = [
+    Student("John", "18001", 9.8),
+    Student("Alice", "18003", 9.2),
+    Student("Bob", "18002", 9.5),
+    Student("Eva", "18005", 8.5),
+    Student("David", "18004", 9.0),
+]
+sort_students(students)
+for student in students:
+    print(student.name, student.roll_number, student.CGPA)
